9d53aca4cf5fe85c55d89a47b027235aef4c40ae
aiqbal84 <51821784+aiqbal84@users.noreply.github.com>
2019-11-01 21:51:19 +0530

    Update App.java

--- src/main/java/com/devops/training/mavendemo/App.java
+++ src/main/java/com/devops/training/mavendemo/App.java
@@ -6,7 +6,7 @@ package com.devops.training.mavendemo;
  */
 public class App 
 {
-    public staticA void main( String[] args )
+    public static void main( String[] args )
     {
         System.out.println( "Hello Asif!" );
     }
